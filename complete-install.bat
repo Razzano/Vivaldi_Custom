403ab1559fc0ba6ec30f2fbdd29dabdee90c7ed0
Sonny <119897675+Razzano@users.noreply.github.com>
2023-07-22 19:43:37 -0400

    Update complete-install.bat

--- complete-install.bat
+++ complete-install.bat
@@ -19,30 +19,38 @@ try {
   write-host "Writing _complete_.css and _complete_.js into vivaldi browser.html file"
   $html | Where-Object { $break -Eq 0 } | ForEach-Object {
     $line = $_
-    if ($line.tolower().contains('<link rel="stylesheet" href="style/_complete_.css" />')) {
+    if ($line.tolower().contains('<link rel="stylesheet" href="style/complete/complete.css" />')) {
       $break = 1;
     } elseif ($line.tolower().contains('</head>')) {
       $writeneeded = 1
-      $outhtml += '    <link rel="stylesheet" href="style/_complete_.css" />'
+      $outhtml += '    <link rel="stylesheet" href="style/complete/complete.css" />'
     }
-    if ($line.tolower().contains('<script src="_complete_.js">')) {
+    if ($line.tolower().contains('<script src="vivaldi-custom-complete.js">')) {
        $break = 1;
     } elseif ($line.tolower().contains('</body>')) {
       $writeneeded = 1
-      $outhtml += '    <script src="_complete_.js"></script>'
+      $outhtml += '    <script src="vivaldi-custom-complete.js"></script>'
     }
     $outhtml += $_
   }
   if ($writeneeded -eq 1) {
     [System.IO.File]::WriteAllLines( (join-path $dstdir "browser.html"), $outhtml, $encoding)
   } else {
-    write-host "The vivaldi browser.html already includes references to _complete_.css and _complete_.js"
+    write-host "The vivaldi browser.html already includes references to complete_.css and vivald-custom-complete_.js"
   }
-  write-host "Copying _complete_.js to vivaldi folder"
-  copy $srcdir\_complete_.js $dstdir
-  write-host "Copying _complete_.css, _*_.png files into style folder"
-  copy $srcdir\_*_.css $dstdir\style
-  copy $srcdir\_*_.png $dstdir\style
+  write-host "Copying vivald-custom-complete_.js to vivaldi folder"
+  copy $srcdir\vivaldi-custom-complete.js $dstdir
+  write-host "Creating sub-folder under style folder"
+  $Path = "$dstdir\style\complete"
+  if (!(Test-Path $Path)) { 
+    New-Item -itemType Directory -Path $Path
+    write-host “Folder created.” 
+  } else { 
+    write-host "Folder already exists!" 
+  } 
+  write-host "Copying files to style\complete folder"
+  copy $srcdir\*.css $dstdir\style\complete
+  copy $srcdir\*.png $dstdir\style\complete
   write-host "Copying Done"
 } catch {
   write-host "Error: " $_
