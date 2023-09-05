a751a691b8a2a466759aa662bb32d8b57242d75d
Sonny <119897675+Razzano@users.noreply.github.com>
2023-09-05 09:56:46 -0400

    Add files via upload

---
+++ custom-ui-mod-Uninstall.bat
@@ -0,0 +1,27 @@
+@set installhooks_args=%*& set installhooks_self=%~f0& powershell -c "(gc \"%~f0\") -replace '@set installhooks_args.*','#' | Write-Host" | powershell -c -& goto :eof
+$srcdir = split-path $env:installhooks_self
+$vivpath = "$env:localappdata\Vivaldi\Application\"
+
+if ($vivpath.tolower().contains("appdata")) {
+  $dstdir = split-path ((Get-ChildItem -path $vivpath -recurse browser.html | Sort-Object -property CreationTime -descending | Select-Object -first 1).FullName)
+} elseif ($vivpath.tolower().contains("program files")) {
+  $dstdir = split-path ((Get-ChildItem -path "C:\Program Files\Vivaldi\Application\" -recurse browser.html | Sort-Object -property CreationTime -descending | Select-Object -first 1).FullName)
+}
+
+if (!(Test-Path -Path $dstdir\custom-ui-mod.js)) { exit }
+
+write-host "Restoring original browser.html and window.html"
+copy $srcdir\browser.html $dstdir\browser.html
+copy $srcdir\window.html $dstdir\window.html
+
+write-host "Deleting custom-ui-mod.js file from vivaldi folder"
+$Custom = "$dstdir\custom-ui-mod.js"
+if (Test-Path -Path $Custom) { del $Custom }
+
+write-host "Replacing vivaldi > style\common.css file back to default"
+$Common = "$dstdir\style\common.css"
+copy $srcdir\common.css $Common
+
+write-host "Done"
+write-host -NoNewLine "Press any key to continue..."
+$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
